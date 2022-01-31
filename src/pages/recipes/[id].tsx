0dae06181da4b97f8bb825adeda27b30b9c84a64
Nicolae Garbalau <nicolae.garbalau@3pillarglobal.com>
2022-01-31 20:27:28 +0200

    Switch to MongoDB API approach

--- src/pages/recipes/[id].tsx
+++ src/pages/recipes/[id].tsx
@@ -1,8 +1,8 @@
 import Nav from '../../components/Nav';
 
 // MongoDB
-// import clientPromise from '../../../lib/mongodb';
-// import { ObjectId } from 'mongodb';
+import clientPromise from '../../../lib/mongodb';
+import { ObjectId } from 'mongodb';
 
 const Recipe = ({ recipe }) => {
   const { image, name, description, ingredients } = recipe;
@@ -32,22 +32,24 @@ const Recipe = ({ recipe }) => {
 
 export async function getServerSideProps(ctx) {
   // MongoDB
-  //   const client = await clientPromise;
-  //   const db = client.db('recipe-db');
-  //   let recipe = await db
-  //     .collection('recipes')
-  //     .findOne({ _id: new ObjectId(ctx.query.id) });
-  //   recipe = JSON.parse(JSON.stringify(recipe));
-  try {
-    const response = await fetch(
-      `${process.env.API_HOST}/details?_id=${ctx.query.id}`
-    );
-    const recipe = await response.json();
-    return {
-      props: { recipe },
-    };
-  } catch (err) {
-    console.log(err);
-  }
+  const client = await clientPromise;
+  const db = client.db('recipe-db');
+  let recipe = await db
+    .collection('recipes')
+    .findOne({ _id: new ObjectId(ctx.query.id) });
+  recipe = JSON.parse(JSON.stringify(recipe));
+
+  // Local API
+  // try {
+  //   const response = await fetch(
+  //     `${process.env.API_HOST}/details?_id=${ctx.query.id}`
+  //   );
+  //   const recipe = await response.json();
+  //   return {
+  //     props: { recipe },
+  //   };
+  // } catch (err) {
+  //   console.log(err);
+  // }
 }
 export default Recipe;
