e66ed4c9874ffdf783408b64f006dcacb1544267
Nicolae Garbalau <nicolae.garbalau@3pillarglobal.com>
2022-01-31 20:11:39 +0200

    Changed import of objects from .env

--- src/pages/api/details.ts
+++ src/pages/api/details.ts
@@ -1,15 +1,14 @@
 import clientPromise from '../../../lib/mongodb';
 import { ObjectId } from 'mongodb';
+import { NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
-  const query = req.query._id;
-
+export default async function handler(req, res: NextApiResponse) {
   const client = await clientPromise;
   const db = client.db('recipe-db');
 
   let recipe = await db
     .collection('recipes')
-    .findOne({ _id: new ObjectId(query) });
+    .findOne({ _id: new ObjectId(req.query._id) });
 
   recipe = JSON.parse(JSON.stringify(recipe));
 
